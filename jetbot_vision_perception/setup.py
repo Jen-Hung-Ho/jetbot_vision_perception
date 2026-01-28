c233e239341c8bb473c1abf2397374f4a80ed552
Jen-Hung-Ho <jenhungho@outlook.com>
2026-01-28 01:25:52 -0800

    Add launch and parameter files for YOLO detection node
    Description:
    - Added yolo_detection.yaml parameter file in param/ directory with default configurations for the YOLO detection node
    - Added yolo_detection.launch.py launch file in launch/ directory to simplify node startup using the parameter file
    - Updated setup.py to include launch/ and param/ directories in package data for proper installation and distribution
    
    	new file:   jetbot_vision_perception/launch/yolo_detection.launch.py
    	new file:   jetbot_vision_perception/param/yolo_detection_params.yaml
    	modified:   jetbot_vision_perception/setup.py

--- jetbot_vision_perception/setup.py
+++ jetbot_vision_perception/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+from glob import glob
+import os
 
 package_name = 'jetbot_vision_perception'
 
@@ -10,6 +12,10 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        # Include all launch files
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        # Include all param files
+        (os.path.join('share', package_name, 'param'), glob('param/*params.yaml')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
